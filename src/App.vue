d0ff105df5085824cdd4270daeb7ae9228cc912f
Andy Chong Sam <achongs.business@gmail.com>
2021-04-11 10:13:24 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <locations></locations>
+</template>
+
+<script>
+import Locations from './components/Locations.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Locations
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
